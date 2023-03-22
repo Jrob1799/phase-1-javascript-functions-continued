dc546ecf2470d1f07aefafc462d700c6015b1d71
Jacob <jacobwroberts33@gmail.com>
2023-03-22 18:43:59 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+function saturdayFun(fun = "roller-skate") {
+    return `This Saturday, I want to ${fun}!`;
+  }
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(highlight = "*") {
+    return function(adj) {
+      return `You are ${highlight}${adj}${highlight}!`;
+    }
+  }
